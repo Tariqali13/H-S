5c550cc5dbcbe69663d9b2f9b2e8b9e53fd65ec3
Muqaddaskhalil08 <93579258+Muqaddaskhalil08@users.noreply.github.com>
2021-11-26 23:29:39 +0500

    all pages done

--- constants/doula-service.js
+++
@@ -1,5 +0,0 @@
-export const tableHeadings = [
-  'Title',
-  'Date',
-  'Actions',
-];
--- constants/secure-template.js
+++ constants/secure-template.js
@@ -12,39 +12,33 @@ const routes = [
     layout: "/admin",
   },
   {
-    path: "/pricing-plans",
-    name: "Pricing Plans",
+    path: "/employees",
+    name: "Employees",
     icon: "ni ni-single-copy-04 text-pink",
     layout: "/admin",
   },
   {
-    path: "/pricing-plan-features",
-    name: "Pricing Plan Features",
-    icon: "ni ni-ui-04 text-pink",
-    layout: "/admin",
-  },
-  {
-    path: "/gallery",
-    name: "Gallery",
+    path: "/employees-videos",
+    name: "Employees Videos",
     icon: "ni ni-image text-yellow",
     layout: "/admin",
   },
   {
-    path: "/categories",
-    name: "Gallery Categories",
+    path: "/consultation",
+    name: "Consultation",
+    icon: "ni ni-ui-04 text-pink",
+    layout: "/admin",
+  },
+  {
+    path: "/products",
+    name: "Products",
     icon: "ni ni-collection text-yellow",
     layout: "/admin",
   },
   {
-    path: "/doula-services",
-    name: "Doula Services",
+    path: "/profile-setting",
+    name: "Profile Setting",
     icon: "ni ni-diamond text-primary",
-    layout: "/admin",
-  },
-  {
-    path: "/blogs",
-    name: "Blogs",
-    icon: "ni ni-paper-diploma text-pink",
     layout: "/admin",
   },
   {
@@ -53,30 +47,12 @@ const routes = [
     icon: "ni ni-satisfied text-black",
     layout: "/admin",
   },
-  // {
-  //   path: "/users",
-  //   name: "Users",
-  //   icon: "ni ni-single-02 text-red",
-  //   layout: "/admin",
-  // },
   {
     path: "/contacts",
     name: "Contacts",
     icon: "ni ni-collection text-info",
     layout: "/admin",
   },
-  {
-    path: "/subscribtions",
-    name: "Subscribtions",
-    icon: "ni ni-support-16 text-primary",
-    layout: "/admin",
-  },
-  // {
-  //   path: "/register",
-  //   name: "Register",
-  //   icon: "ni ni-circle-08 text-pink",
-  //   layout: "/auth-template",
-  // },
 ];
 
 export default routes;
