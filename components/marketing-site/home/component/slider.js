f14a68242ca2d27f05c7fcdcad1b5a22b1b0f92b
Tariqali13 <Tariqali99.ta@gmail.com>
2021-12-08 12:34:16 +0500

    updates

--- components/marketing-site/home/component/slider.js
+++ components/marketing-site/home/component/slider.js
@@ -4,40 +4,65 @@ import {Carousel} from "react-bootstrap";
 const Slider = () => {
   return (
     <div className="slider">
-      <Carousel fade indicators={false}>
+      <Carousel fade indicators={false} interval={null}>
         <Carousel.Item>
-          <img
-            className="d-block w-100 home-slider-img"
-            src="/images/bg-1.jpeg"
-            alt="First slide"
-          />
+          <div className="flex-div">
+            <img
+              className="d-block w-100 home-slider-img blur-image"
+              src="/images/slider-6.jpeg"
+              alt="First slide"
+            />
+            <img
+              className="d-block home-slider-img main-image"
+              src="/images/slider-6.jpeg"
+              alt="First slide"
+            />
+          </div>
           <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <div className="slider-caption">
+            <h3>House with Solar</h3>
+            <p>Industry Leading Equipment and Materials Ensure The Highest Quality Installation Possible</p>
+            </div>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="d-block w-100 home-slider-img"
-            src="/images/2.jpg"
-            alt="Second slide"
-          />
-
+          <div className="flex-div">
+            <img
+              className="d-block w-100 home-slider-img blur-image"
+              src="/images/slider-1.jfif"
+              alt="Second slide"
+            />
+            <img
+              className="d-block home-slider-img main-image"
+              src="/images/slider-1.jfif"
+              alt="Second slide"
+            />
+          </div>
           <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <div className="slider-caption">
+            <h3>Ground Mounted Solar</h3>
+            <p>Ground Mounted Solar For Areas Where the Roof Of The Home Isn't The Ideal Location</p>
+            </div>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="d-block w-100 home-slider-img"
-            src="/images/3.jpg"
-            alt="Third slide"
-          />
-
+          <div className="flex-div">
+            <img
+              className="d-block w-100 home-slider-img blur-image"
+              src="/images/slider-3.jpg"
+              alt="Third slide"
+            />
+            <img
+              className="d-block home-slider-img main-image"
+              src="/images/slider-3.jpg"
+              alt="Third slide"
+            />
+          </div>
           <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+            <div className="slider-caption">
+              <h3>Electric Box</h3>
+              <p>Wide Array Of Electrical Services To Cover Your Needs</p>
+            </div>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
